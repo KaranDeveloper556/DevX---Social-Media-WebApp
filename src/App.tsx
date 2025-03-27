c15ab8d0823809ad42b6a034c1ec73b1501b2a51
Karan bohra <its.karanbohra556@gmail.com>
2025-03-28 03:24:09 +0530

    Pages Routing Compeleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import SignInForm from "./_auth/forms/SignInForm";
-import { Home } from "./_root/pages";
+import {
+  Home,
+  Explore,
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Profile,
+  UpdateProfile,
+  PostDetails,
+  Saved,
+  LikedPost,
+} from "./_root/pages";
 import SignUpForm from "./_auth/forms/SignUpForm";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
@@ -19,14 +30,15 @@ const App = () => {
         {/* Private Routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
-          {/* <Route path="/explore" element={<Home />} />
-          <Route path="/saved" element={<Explore />} />
-          <Route path="/all-users" element={<Saved />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
           <Route path="/create-post" element={<CreatePost />} />
           <Route path="/update-post/:id" element={<EditPost />} />
           <Route path="/post/id" element={<PostDetails />} />
           <Route path="/porfile/:id/*" element={<Profile />} />
-          <Route path="/update-profile/:id" element={<UpdateProfile />} /> */}
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
+          <Route path="/update-prof" element={<LikedPost />} />
         </Route>
       </Routes>
       <ToastContainer />
