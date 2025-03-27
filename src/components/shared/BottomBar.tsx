699a0350da6a0ad7f60dbfda9a3d948fa5644124
Karan bohra <its.karanbohra556@gmail.com>
2025-03-28 03:44:07 +0530

    Bottom Bar Compeleted

--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { bottombarLinks } from "@/constants";
+import { Link, useLocation } from "react-router-dom";
 
 const BottomBar = () => {
+  const { pathname } = useLocation();
   return (
-    <div>BottomBar</div>
-  )
-}
+    <section className="bottom-bar z-50 flex justify-center gap-2 w-full sticky bottom-0 rounded-t-[10px] px-4 py-2 md:hidden bg-gray-900">
+      {bottombarLinks.map((link) => {
+        const isActive = pathname === link.route;
+        return (
+          <li
+            key={link.label}
+            className={`rounded-lg base-medium hover:bg-blue-700 transition group list-none ${
+              isActive && "bg-blue-700"
+            }`}
+          >
+            <Link to={link.route} className="flex flex-col gap-1 items-center p-4">
+              <img
+                src={link.imgURL}
+                alt={link.label}
+                className={`group-hover:invert-white h-[20px]`}
+              />
+              <p className="text-[10px] font-semibold">{link.label}</p>
+              
+            </Link>
+          </li>
+        );
+      })}
+    </section>
+  );
+};
 
-export default BottomBar
+export default BottomBar;
